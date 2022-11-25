c3a9c29e3b5811f94027a2477d7f35d2051e6708
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2022-11-24 20:45:16 -0500

    Add files via upload

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -1,29 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
+let tweets = [
+    {
+        "name": "Codr Kai",
+        "msg": "This is my first tweet!",
+        "username": "codrkai"
+    },
+    {
+        "name": "Samantha Kai",
+        "msg": "React JS is so simple!",
+        "username": "samanthakai"
+    },
+    {
+        "name": "John K",
+        "msg": "Sweep the leg!",
+        "username": "johnk"
+    }
+];
 router.get('/tweets', (req, res) => {
-    const str = [
-        {
-            "name": "Codr Kai",
-            "msg": "This is my first tweet!",
-            "username": "codrkai"
-        },
-        {
-            "name": "Samantha Kai",
-            "msg": "React JS is so simple!",
-            "username": "samanthakai"
-        },
-        {
-            "name": "John K",
-            "msg": "Sweep the leg!",
-            "username": "johnk"
-        }
-    ];
-    res.end(JSON.stringify(str));
+    res.end(JSON.stringify(tweets));
 });
 
 router.post('/addTweet', (req, res) => {
-    res.end('NA');
+    let json = req.body;
+    console.log(json)
+    if(json.name == null || json.msg == null || json.username == null) {
+        res.status(400).send('Bad Request');
+        return;
+    }
+    tweets.unshift({"name": json.name, "msg": json.msg, "username": json.username})
 });
 
 module.exports = router;
