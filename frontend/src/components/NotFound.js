d34529ad9f51182bbc3c904abe886953d555e3f5
Declipsonator <96392121+Declipsonator@users.noreply.github.com>
2022-11-24 20:43:48 -0500

    Add files via upload

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function NotFound() {
+    return(
+        <section>
+            <div class="container-fluid">
+                <h1 class="mt-5">404</h1>
+                <p>What are you looking for?</p>
+            </div>
+        </section>
+    );
+}
+
+export default NotFound;
--- frontend/src/components/Tweet.js
+++ frontend/src/components/Tweet.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-// import {Link} from 'react-router-dom';
+
 
 function Tweet() {
     useEffect( () => {
@@ -15,24 +15,47 @@ function Tweet() {
     };
 
     return(
-        <section>
-            {
-            items.map(item => (
-                <div class="container-fluid p-3 w-50">
-                    <div class="card-deck">
-                        <div class="card">
-                            <div class="card-body p-1">
-                                <h6 class="card-title">{item.name}</h6>
-                                <p class="card-text">{item.msg}</p>
-                                <p class="card-text"><i>by {item.username}</i></p>
+        <div>
+            <input id='name-input' placeholder='name' type="text"/>
+            <input id='message-input' placeholder='message' type="text"/>
+            <input id='username-input' placeholder='username' type="text"/>
+            <button value=" Submit "  onClick={
+                function submitData() {
+                    let json = {"name": document.getElementById("name-input").value, "msg": document.getElementById("message-input").value, "username": document.getElementById("username-input").value};
+                    console.log(JSON.stringify(json));
+                    fetch('/addtweet', {  // Enter your IP address here
+                    method: 'POST', 
+                    mode: 'cors',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(json) // body data type must match "Content-Type" header
+                  })
+                }
+            }> Submit </button>
+            <section>
+                {
+                items.map(item => {
+                    return (
+                    <div class="container-fluid p-3 w-50">
+                        <div class="card-deck">
+                            <div class="card">
+                                <div class="card-body p-1">
+                                    <h6 class="card-title">{item.name}</h6>
+                                    <p class="card-text">{item.msg}</p>
+                                    <p class="card-text"><i>by {item.username}</i></p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            ))
-            }
-        </section>
+                    )
+                    })
+                }
+            </section>
+        </div>
     );
 }
+
+
 
 export default Tweet;
